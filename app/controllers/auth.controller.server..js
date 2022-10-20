e16ee48bd9110f69a9cd1d1eb9b996b1a99cb084
YouMadBro!? <amaynar4@my.centennialcollege.ca>
2022-10-20 13:03:42 -0500

    created utils index.js and started authentication

---
+++ app/controllers/auth.controller.server..js
@@ -0,0 +1,18 @@
+//File: auth.controller.server.js
+//Student Name: Alexander Maynard
+//Student ID: 301170707
+//Date: 2022-10-20
+
+
+//import express
+import express from 'express';
+
+//import passport for authentication
+import passport from 'passport';
+
+// import UsersModel for authentication
+import { UserDisplayName } from '../utils/index.js';
+
+
+
+
--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
 /*File: config.js
 Student Name: Alexander Maynard
 Student ID: 301170707
-Date: 2022-10-01*/
+Date: 2022-10-20*/
 
 //exports SecretCode for use in app.js
 export const SecretCode = "301170707";
+export const MOngoURI = "mongodb+srv://almaynard2404:ah0N4u4ZIccpIm0l@comp229-fall2022.mqk9wbc.mongodb.net/?retryWrites=true&w=majority";
